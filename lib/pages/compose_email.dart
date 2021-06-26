8e1e6fe1e0e0a3f5cd65fb602d8e81204f844705
Gaurav Viramgami <viramgami.g@iitgn.ac.in>
2021-06-27 03:52:22 +0530

    Compose Page

--- lib/pages/compose_email.dart
+++ lib/pages/compose_email.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_summernote/flutter_summernote.dart';
 
 class ComposeEmailPage extends StatefulWidget {
   const ComposeEmailPage({ Key? key }) : super(key: key);
@@ -19,6 +20,30 @@ class _ComposeEmailPageState extends State<ComposeEmailPage> {
     }
   }
 
+  GlobalKey<FlutterSummernoteState> _keyEditor = GlobalKey();
+  late TextEditingController _toController;
+  late TextEditingController _subjectController;
+  late TextEditingController _ccController;
+  late TextEditingController _bccController;
+
+  @override
+  void initState() {
+    super.initState();
+    _toController = TextEditingController();
+    _subjectController = TextEditingController();
+    _ccController = TextEditingController();
+    _bccController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _toController.dispose();
+    _subjectController.dispose();
+    _ccController.dispose();
+    _bccController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -36,9 +61,98 @@ class _ComposeEmailPageState extends State<ComposeEmailPage> {
           ),
         ],
       ),
-      body: Center(
-        child: Container(
-          child: Text("Compose your email"),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 20,),
+              Text(
+                  'To:',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 10,),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'To',
+                ),
+                controller: _toController,
+              ),
+              SizedBox(height: 20,),
+              Text(
+                'Subject:',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 10,),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Subject',
+                ),
+                controller: _subjectController,
+              ),
+              SizedBox(height: 20,),
+              Text(
+                'cc:',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 10,),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'cc',
+                ),
+                controller: _ccController,
+              ),
+              SizedBox(height: 20,),
+              Text(
+                'bcc:',
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+              SizedBox(height: 10,),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'bcc',
+                ),
+                controller: _bccController,
+              ),
+              SizedBox(height: 20,),
+              FlutterSummernote(
+                  hint: "Your Mail Body here...",
+                  key: _keyEditor
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlinedButton.icon(
+                    label: Text('Send'),
+                    icon: Icon(Icons.send,),
+                    onPressed: () {
+                      //TODO: Send Mail
+                    },
+                  ),
+                  OutlinedButton.icon(
+                    label: Text('Schedule'),
+                    icon: Icon(Icons.schedule,),
+                    onPressed: () {
+                      //TODO: Schedule Mail
+                    },
+                  )
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
