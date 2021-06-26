367dd53f7d770b6721fe8d50f702223ac0d765e7
Gaurav Viramgami <viramgami.g@iitgn.ac.in>
2021-06-27 01:08:17 +0530

    Stop Mails

--- lib/data/history_data.dart
+++ lib/data/history_data.dart
@@ -136,5 +136,55 @@ Future<bool> scheduleMail(mailData) async {
   }
 }
 
+Future<bool> cancelMail(id) async {
+  try {
+    if(emailid == "none"){
+      emailid = FirebaseAuth.instance.currentUser!.email!;
+      if(emailid.endsWith("@codebois.com")){
+        googlesigninornot = false;
+      }
+    }
+
+    if(uid == "none"){
+      uid = FirebaseAuth.instance.currentUser!.uid;
+    }
+
+    if (googlesigninornot == true && emailid != "none" && uid != "none") {
+      print(accesstoken);
+      print(emailid);
+      print(uid);
+      print(googlesigninornot);
+      var queryParameters = {
+        'api_key': 'GAURAV',
+        'id': id
+      };
+
+      var baseUrl = "stark-castle-06298.herokuapp.com";
+
+      var uri = Uri.https(baseUrl, '/stopMail', queryParameters);
+      print("PINGING:" + uri.toString());
+
+      var response = await http.get(uri);
+      Map<String, dynamic> responseJson = jsonDecode(response.body);
+
+      if(responseJson['success'] == true){
+        print("Mails stopped");
+        return true;
+      }
+      else {
+        print(responseJson['message']);
+        return false;
+      }
+    }
+    else {
+      return false;
+    }
+
+  } on Exception catch (e) {
+    print(e);
+    return false;
+  }
+}
+
 //var url = Uri.parse("https://stark-castle-06298.herokuapp.com/getMails?api_key=GAURAV&email_id=gauravhviramgami@gmail.com");
 //var url = https://stark-castle-06298.herokuapp.com/scheduleMail?api_key=GAURAV&email_id=reubendevanesan@gmail.com&tocken=ya29.a0ARrdaM-nBR-ZM7MYw6eBzPtXYTWD7fLJm8yHj2ha_iGsV6yGMnJFsosTG46omDp5-B9EE3WujLFhDyTEtHV7ZIsJPW_xYO7WSqiP1Tbs9KSVLnxDa7bdDIA_f7rO5GAjo4iSrr7xkuTAxRrk8uGfEhlEg-Ol&to=viramgami.g@iitgn.ac.in&cc=reuben.sd@iitgn.ac.in&from=reubendevanesan@gmail.com&subject=This is a test subject&html=<h1>Hello! This is test Mail</h1>
