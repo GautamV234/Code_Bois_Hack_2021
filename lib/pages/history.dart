c2998dbed1fc4be1a2a663b82de3d44aad228939
Gaurav Viramgami <viramgami.g@iitgn.ac.in>
2021-06-27 00:41:32 +0530

    History Page

--- lib/pages/history.dart
+++ lib/pages/history.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
 import '../data/history_data.dart';
@@ -28,9 +29,7 @@ class _HistoryPageState extends State<HistoryPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: (loading) ? LoadingWidget() : SingleChildScrollView(child: showHistoryData())
-    );
+    return (loading) ? LoadingWidget() : SingleChildScrollView(child: showHistoryData());
   }
 }
 
@@ -40,12 +39,83 @@ Widget showHistoryData(){
   for(var i = 0; i < historyData.length; i++){
     history.add(
       ExpansionTile(
-        title: Text('${historyData[i]['to']}'),
-        children: <Widget>[
-          Html(
-            data: historyData[i]['html_body']
+        title: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text(
+                      'To: ',
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                  Text(historyData[i]['to'].join(', ')),
+                ],
+              ),
+              Text(
+                '${historyData[i]['subject']}',
+                style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 14,
+                ),
+              )
+            ],
           ),
-          Text('${historyData[i]['subject']}'),
+        ),
+        children: <Widget>[
+          (historyData[i]['cc'].length == 0)
+              ? Container()
+              : Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 2.0, 12.0, 2.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'cc: ',
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Text(
+                        historyData[i]['cc'].join(', '),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          (historyData[i]['bcc'].length == 0)
+              ? Container()
+              : Padding(
+                padding: const EdgeInsets.fromLTRB(12.0, 2.0, 12.0, 2.0),
+                child: Row(
+                  children: [
+                    Text(
+                      'bcc: ',
+                      style: TextStyle(
+                        fontStyle: FontStyle.italic,
+                      ),
+                    ),
+                    Text(
+                      historyData[i]['bcc'].join(', '),
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+          Padding(
+            padding: const EdgeInsets.all(6.0),
+            child: Html(
+              data: historyData[i]['html_body']
+            ),
+          ),
         ],
       )
     );
